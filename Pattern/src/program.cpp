971e91bb4df111f6a5b37733486953c63fec11d9
Alecks <ai.zassst@gmail.com>
2020-09-18 11:58:26 +0300

    Pattern update

---
+++ Pattern/src/program.cpp
@@ -0,0 +1,47 @@
+
+#include "head.h"
+
+
+using namespace std;
+
+
+
+int main(int argc, char** args)
+{
+
+Path("Path 1");    
+/* Задание №1 
+
+*/
+
+
+Path("Path 2");
+/* Задание №2
+
+*/
+
+ 	
+
+Path("Path 3");
+/* Задание №3
+
+*/
+
+ 	
+Path("END");
+
+
+system("pause"); // Для паузы при запуске файла .exe из системы
+    return 0;					
+
+
+}
+
+
+
+
+
+
+
+
+
